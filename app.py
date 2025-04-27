5bbcbd8e28a33a656bc0cef4985ad1bc2e2a9fbd
Bhavish P K <71438365+bpk18@users.noreply.github.com>
2025-04-27 15:14:56 +0530

    app.py

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def excel_to_pdf():
     return render_template('excel_to_pdf.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)  # Run on port 5001 instead of the default 5000
+    app.run(debug=True, host='0.0.0.0', port=5000) 
 
