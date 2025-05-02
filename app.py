bb66b40cb4638e56f6a20a04ae26f6d3e67527a0
Bhavish P K <71438365+bpk18@users.noreply.github.com>
2025-05-02 15:38:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, send_file, redirect, url_for
 from werkzeug.utils import secure_filename
-import pypdf
 from PIL import Image
 from fpdf import FPDF
 import os
