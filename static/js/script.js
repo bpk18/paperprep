8378bf105da1d8657bb7fffb7911562656604704
Bhavish P K <71438365+bpk18@users.noreply.github.com>
2025-05-02 14:47:31 +0530

    Delete static directory

--- static/js/script.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const toggle = document.getElementById("theme-toggle");
-  toggle.addEventListener("click", () => {
-    fetch("/toggle-theme").then(() => location.reload());
-  });
-});
