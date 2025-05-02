9dc0550a56b003f1ea843bed144a3a0b39476e8b
Bhavish P K <71438365+bpk18@users.noreply.github.com>
2025-05-02 19:56:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -470,4 +470,4 @@ def terms():
     return render_template_string(simple_page_template, title="Terms and Conditions", content=content)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
