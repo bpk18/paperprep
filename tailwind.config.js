7994714110ea582d499c6d696fe9e8ea01b9d062
Bhavish P K <71438365+bpk18@users.noreply.github.com>
2026-01-09 09:51:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './**/*.py'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'Segoe UI', 'system-ui']
+      }
+    }
+  },
+  plugins: []
+};
